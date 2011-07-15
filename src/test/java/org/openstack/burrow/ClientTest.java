6e8c3f997a86af589e52fbd57d39da0a42ca52a9
Erik Swanson <erik.swanson@gmail.com>
2011-07-14 18:43:51 -0700

    Replaced client tests with functional tests that verify behavior.

--- src/test/java/org/openstack/burrow/ClientTest.java
+++ src/test/java/org/openstack/burrow/ClientTest.java
@@ -1,12 +1,12 @@
 /*
  * Copyright (C) 2011 OpenStack LLC.
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -16,9 +16,12 @@
 
 package org.openstack.burrow;
 
+import java.util.List;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+
 import org.openstack.burrow.backend.Backend;
 import org.openstack.burrow.backend.Http;
 import org.openstack.burrow.client.Account;
@@ -47,7 +50,7 @@ public class ClientTest extends TestCase {
   }
 
   /**
-   * Create and then get message.
+   * Create and then get a message.
    */
   public void testCreateGetMessage() {
     String id = "testCreateGetMessage";
@@ -58,148 +61,23 @@ public class ClientTest extends TestCase {
   }
 
   /**
-   * Create a message with a hide.
+   * Create two messages and then verify their presence in getMessages.
    */
-  public void testCreateMessageSetHide() {
-    queue.createMessage("messageIdWithHide", "messageBody").setHide(900).execute();
-  }
-
-  /**
-   * Create a message with a ttl.
-   */
-  public void testCreateMessageSetTtl() {
-    queue.createMessage("messageIdWithTtl", "messageBody").setTtl(900).execute();
-  }
-
-  /**
-   * Create a message with a ttl and a hide.
-   */
-  public void testCreateMessageSetTtlSetHide() {
-    queue.createMessage("messageIdWithHideAndTtl", "messageBody").setTtl(900).setHide(20).execute();
-  }
-
-  public void testDeleteAccounts() {
-    client.deleteAccounts().execute();
-  }
-
-  public void testDeleteAccountsMatchLimit() {
-    client.deleteAccounts().matchLimit(1892L).execute();
-  }
-
-  public void testDeleteAccountsWithMarker() {
-    client.deleteAccounts().withMarker("marker").execute();
-  }
-
-  public void testDeleteMessage() {
-    queue.deleteMessage("messageId").execute();
-  }
-
-  public void testDeleteMessageMatchHidden() {
-    queue.deleteMessage("messageId").matchHidden(true).execute();
-  }
-
-  public void testDeleteMessages() {
-    queue.deleteMessages().execute();
-  }
-
-  public void testDeleteMessagesRequestWait() {
-    queue.deleteMessages().requestWait(18932038403L).execute();
-  }
-
-  public void testDeleteMessagesRequestDetail() {
-    queue.deleteMessages().requestDetail("detail").execute();
-  }
-
-  public void testDeleteQueues() {
-    account.deleteQueues().execute();
-  }
-
-  public void testDeleteQueuesRequestDetail() {
-    account.deleteQueues().requestDetail("detail").execute();
-  }
-
-  public void testDeleteQueuesWithMarker() {
-    account.deleteQueues().withMarker("marker").execute();
-  }
-
-  public void testGetAccounts(){
-    client.getAccounts().execute();
-  }
-
-  public void testGetAccountsMatchLimit(){
-    client.getAccounts().matchLimit(1892L).execute();
-  }
-
-  public void testGetAccountsWithMarker(){
-    client.getAccounts().withMarker("marker").execute();
-  }
-
-
-  public void testGetMessage(){
-    // queue.getMessage("MessageId").execute();
-  }
-
-  public void testGetMessageRequestDetail(){
-    // queue.getMessage("MessageId").requestDetail("detail").execute();
-  }
-
-  public void testGetMessages(){
-    queue.getMessages().execute();
-  }
-
-  public void testGetMessagesMatchHidden(){
-    queue.getMessages().matchHidden(true).execute();
-  }
-
-  public void testGetMessagesMatchLimit(){
-    queue.getMessages().matchLimit(1892L).execute();
-  }
-
-  public void testGetMessagesWithMarker(){
-    queue.getMessages().withMarker("marker").execute();
-  }
-
-  public void testGetQueues(){
-    account.getQueues().execute();
-  }
-
-  public void testGetQueuesMatchLimit(){
-    account.getQueues().matchLimit(172L).execute();
-  }
-
-  public void testGetQueuesWithMarker(){
-    account.getQueues().withMarker("marker").execute();
-  }
-
-  public void testUpdateMessage(){
-    queue.updateMessage("messageId").execute();
-  }
-
-  public void testUpdateMessageSetHide(){
-    queue.updateMessage("messageId").setHide(900).execute();
-  }
-
-  public void testUpdateMessageSetTtl(){
-    queue.updateMessage("messageId").setTtl(900).execute();
-  }
-
-  public void testUpdateMessageSetTtlSetHide(){
-    queue.updateMessage("messageId").setTtl(900).setHide(900).execute();
-  }
-
-  public void testUpdateMessages(){
-    queue.updateMessages().execute();
-  }
-
-  public void testUpdateMessagesSetHide(){
-    queue.updateMessages().setHide(00).execute();
-  }
-
-  public void testUpdateMessagesSetTtl(){
-    queue.updateMessages().setTtl(100).execute();
-  }
-
-  public void testUpdateMessagesSetHideSetTtl(){
-    queue.updateMessages().setHide(900).setTtl(100).execute();
+  public void testCreateGetMessages() {
+    String id1 = "testCreateGetMessages1";
+    String id2 = "testCreateGetMessages2";
+    String body = "testCreateGetMessagesBody";
+    boolean seen_id1 = false;
+    boolean seen_id2 = false;
+    queue.createMessage(id1, body).execute();
+    queue.createMessage(id2, body).execute();
+    List<Message> messages = queue.getMessages().execute();
+    for (Message message : messages) {
+      if (message.getId().equals(id1))
+        seen_id1 = true;
+      else if (message.getId().equals(id2))
+        seen_id2 = true;
+    }
+    assertTrue(seen_id1 && seen_id2);
   }
 }
