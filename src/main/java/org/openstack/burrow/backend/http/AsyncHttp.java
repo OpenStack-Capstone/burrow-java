2020649140af13e90440bc79a45d3554d827e9f2
Justin Wagner <jwagne@pdx.edu>
2011-07-31 15:06:34 -0700

    AsyncHttp which implements AsyncBackend added

---
+++ src/main/java/org/openstack/burrow/backend/http/AsyncHttp.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright (C) 2011 OpenStack LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.openstack.burrow.backend.http;
+
+import org.openstack.burrow.backend.AsyncBackend;
+import org.openstack.burrow.client.Account;
+import org.openstack.burrow.client.Message;
+import org.openstack.burrow.client.Queue;
+import org.openstack.burrow.client.methods.*;
+
+import java.util.List;
+
+public class AsyncHttp implements AsyncBackend{
+
+    @Override
+    public Message executeAsync(CreateMessage request) {
+        return null;
+    }
+
+    @Override
+    public Message executeAsync(DeleteMessage request) {
+        return null;
+    }
+
+    @Override
+    public List<Message> executeAsync(DeleteMessages request) {
+        return null;
+    }
+
+    @Override
+    public Message executeAsync(GetMessage request) {
+        return null;
+    }
+
+    @Override
+    public List<Message> executeAsync(GetMessages request) {
+        return null;
+    }
+
+    @Override
+    public Message executeAsync(UpdateMessage request) {
+        return null;
+    }
+
+    @Override
+    public List<Message> executeAsync(UpdateMessages request) {
+        return null;
+    }
+
+    @Override
+    public List<Account> deleteAccounts(String marker, Long limit, String detail) {
+        return null;
+    }
+
+    @Override
+    public List<Queue> deleteQueues(String account, String marker, Long limit, String detail) {
+        return null;
+    }
+
+    @Override
+    public Message execute(CreateMessage request) {
+        return null;
+    }
+
+    @Override
+    public Message execute(DeleteMessage request) {
+        return null;
+    }
+
+    @Override
+    public List<Message> execute(DeleteMessages request) {
+        return null;
+    }
+
+    @Override
+    public Message execute(GetMessage request) {
+        return null;
+    }
+
+    @Override
+    public List<Message> execute(GetMessages request) {
+        return null;
+    }
+
+    @Override
+    public Message execute(UpdateMessage request) {
+        return null;
+    }
+
+    @Override
+    public List<Message> execute(UpdateMessages request) {
+        return null;
+    }
+
+    @Override
+    public List<Account> getAccounts(String marker, Long limit) {
+        return null;
+    }
+
+    @Override
+    public List<Queue> getQueues(String account, String marker, Long limit) {
+        return null;
+    }
+}
+
