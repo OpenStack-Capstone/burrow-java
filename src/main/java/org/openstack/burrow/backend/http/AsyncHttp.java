85f889324f225e910f76b96385e53af1637c14aa
Justin Wagner <jwagne@pdx.edu>
2011-07-31 15:50:11 -0700

    Added a constructor and implemented execute methods to call executeAsync

--- src/main/java/org/openstack/burrow/backend/http/AsyncHttp.java
+++ src/main/java/org/openstack/burrow/backend/http/AsyncHttp.java
@@ -16,6 +16,9 @@
 
 package org.openstack.burrow.backend.http;
 
+import org.apache.http.impl.nio.client.DefaultHttpAsyncClient;
+import org.apache.http.nio.client.HttpAsyncClient;
+import org.apache.http.nio.reactor.IOReactorException;
 import org.openstack.burrow.backend.AsyncBackend;
 import org.openstack.burrow.client.Account;
 import org.openstack.burrow.client.Message;
@@ -23,100 +26,162 @@ import org.openstack.burrow.client.Queue;
 import org.openstack.burrow.client.methods.*;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 public class AsyncHttp implements AsyncBackend{
+  private HttpAsyncClient client;
+  private String host;
+  private int port;
+  private String scheme = "http";
 
+  public AsyncHttp (String host, int port) {
+    this.host = host;
+    this.port = port;
+    try {
+      this.client = new DefaultHttpAsyncClient();
+    } catch (IOReactorException e) {
+      e.printStackTrace();
+      }
+  }
 
+  @Override
+  public List<Account> deleteAccounts(String marker, Long limit, String detail) {
+    return null;
+  }
 
-    @Override
-    public List<Account> deleteAccounts(String marker, Long limit, String detail) {
-        return null;
+  @Override
+  public List<Queue> deleteQueues(String account, String marker, Long limit, String detail) {
+    return null;
+  }
+
+  @Override
+  public Message execute(CreateMessage request) {
+    try {
+      return executeAsync(request).get();
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      } catch (ExecutionException e) {
+        e.printStackTrace();
     }
+    return null;
+  }
 
-    @Override
-    public List<Queue> deleteQueues(String account, String marker, Long limit, String detail) {
-        return null;
+  @Override
+  public Message execute(DeleteMessage request) {
+    try {
+      return executeAsync(request).get();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    } catch (ExecutionException e) {
+      e.printStackTrace();
     }
+    return null;
+  }
 
-    @Override
-    public Message execute(CreateMessage request) {
-        return null;
+  @Override
+  public List<Message> execute(DeleteMessages request) {
+    try {
+      return executeAsync(request).get();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    } catch (ExecutionException e) {
+      e.printStackTrace();
     }
+    return null;
+  }
 
-    @Override
-    public Message execute(DeleteMessage request) {
-        return null;
+  @Override
+  public Message execute(GetMessage request) {
+    try {
+      return executeAsync(request).get();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    } catch (ExecutionException e) {
+      e.printStackTrace();
     }
+    return null;
+  }
 
-    @Override
-    public List<Message> execute(DeleteMessages request) {
-        return null;
+  @Override
+  public List<Message> execute(GetMessages request) {
+    try {
+      return executeAsync(request).get();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    } catch (ExecutionException e) {
+      e.printStackTrace();
     }
+    return null;
+  }
 
-    @Override
-    public Message execute(GetMessage request) {
-        return null;
+  @Override
+  public Message execute(UpdateMessage request) {
+    try {
+      return executeAsync(request).get();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    } catch (ExecutionException e) {
+      e.printStackTrace();
     }
+    return null;
+  }
 
-    @Override
-    public List<Message> execute(GetMessages request) {
-        return null;
+  @Override
+  public List<Message> execute(UpdateMessages request) {
+    try {
+      return executeAsync(request).get();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    } catch (ExecutionException e) {
+      e.printStackTrace();
     }
+    return null;
+  }
 
-    @Override
-    public Message execute(UpdateMessage request) {
-        return null;
-    }
+  @Override
+  public List<Account> getAccounts(String marker, Long limit) {
+    return null;
+  }
 
-    @Override
-    public List<Message> execute(UpdateMessages request) {
-        return null;
-    }
+  @Override
+  public List<Queue> getQueues(String account, String marker, Long limit) {
+    return null;
+  }
 
-    @Override
-    public List<Account> getAccounts(String marker, Long limit) {
-        return null;
-    }
+  @Override
+  public Future<Message> executeAsync(CreateMessage request) {
+    return null;
+  }
 
-    @Override
-    public List<Queue> getQueues(String account, String marker, Long limit) {
-        return null;
-    }
+  @Override
+  public Future<Message> executeAsync(DeleteMessage request) {
+    return null;
+  }
 
-    @Override
-    public Future<Message> executeAsync(CreateMessage request) {
-        return null;
-    }
+  @Override
+  public Future<List<Message>> executeAsync(DeleteMessages request) {
+    return null;
+  }
 
-    @Override
-    public Future<Message> executeAsync(DeleteMessage request) {
-        return null;
-    }
+  @Override
+  public Future<Message> executeAsync(GetMessage request) {
+    return null;
+  }
 
-    @Override
-    public Future<List<Message>> executeAsync(DeleteMessages request) {
-        return null;
-    }
+  @Override
+  public Future<List<Message>> executeAsync(GetMessages request) {
+    return null;
+  }
 
-    @Override
-    public Future<Message> executeAsync(GetMessage request) {
-        return null;
-    }
+  @Override
+  public Future<Message> executeAsync(UpdateMessage request) {
+    return null;
+  }
 
-    @Override
-    public Future<List<Message>> executeAsync(GetMessages request) {
-        return null;
-    }
-
-    @Override
-    public Future<Message> executeAsync(UpdateMessage request) {
-        return null;
-    }
-
-    @Override
-    public Future<List<Message>> executeAsync(UpdateMessages request) {
-        return null;
-    }
+  @Override
+  public Future<List<Message>> executeAsync(UpdateMessages request) {
+    return null;
+  }
 }
 
