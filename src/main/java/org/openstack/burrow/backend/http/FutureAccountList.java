9e5f7c4ac842b4059e09dfd9aeb346304ba5f503
Justin Wagner <jwagne@pdx.edu>
2011-07-31 21:48:19 -0700

    Future classes get() and isdone() methods implemented, license added

--- src/main/java/org/openstack/burrow/backend/http/FutureAccountList.java
+++ src/main/java/org/openstack/burrow/backend/http/FutureAccountList.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2011 OpenStack LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package org.openstack.burrow.backend.http;
 
 import org.apache.http.HttpResponse;
@@ -12,6 +27,7 @@ import java.util.concurrent.TimeoutException;
 public class FutureAccountList implements Future<List<Account>> {
     private HttpResponse httpResponse;
     private Exception exception;
+    private List<Account> accountList;
 
     FutureAccountList () {
     }
@@ -27,16 +43,52 @@ public class FutureAccountList implements Future<List<Account>> {
     }
 
     @Override
-    public boolean isDone() {
+    public synchronized boolean isDone() {
+        if (this.exception != null) {
+            return true;
+        }
+        else if (this.accountList != null) {
+            return true;
+        }
+        else if (this.httpResponse != null) {
+            //TODO: Process response
+            return true;
+        }
         return false;
     }
 
     @Override
     public synchronized List<Account> get() throws InterruptedException, ExecutionException {
-        while (httpResponse == null){
-            this.wait();
+       while (true) {
+            if (this.exception != null) {
+                // Something went wrong and we have an exception.
+                try {
+                    throw this.exception;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else if (this.accountList != null) {
+                // There is a message already here and processed.
+                return this.accountList;
+            } else if (this.httpResponse != null) {
+                // We have been woken because there is a httpResponse and it has not been processed yet.
+                try {
+                    //TODO: Write when Account Http handle is written
+                    // this.accountList = (this.httpResponse);
+                    this.notifyAll();
+                    return this.accountList;
+                    //TODO: Catch and Throw appropriate exceptions
+                } catch (Exception e) {
+                    this.exception = new ExecutionException(new RuntimeException("An error arose:" + e));                    this.notifyAll();
+                    try {
+                        throw this.exception;
+                    } catch (Exception ex) {
+                        e.printStackTrace();
+                    }
+                }
+            } else
+                this.wait();
         }
-        return null;
     }
 
     @Override
@@ -45,14 +97,12 @@ public class FutureAccountList implements Future<List<Account>> {
     }
 
     public synchronized void setHttpResponse(HttpResponse response){
-        this.notify();
         httpResponse = response;
-        this.notifyAll();
+        this.notify();
     }
 
     public synchronized void setException(Exception e) {
-        this.notify();
         exception = e;
-        this.notifyAll();
+        this.notify();
     }
 }
