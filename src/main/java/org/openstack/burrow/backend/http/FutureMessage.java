51dcb19644da3a5916b7ca6903b5bd6380100789
Erik Swanson <erik.swanson@gmail.com>
2011-08-01 14:48:27 -0700

    Cleaned up Future* implementations a bit.

--- src/main/java/org/openstack/burrow/backend/http/FutureMessage.java
+++ src/main/java/org/openstack/burrow/backend/http/FutureMessage.java
@@ -1,12 +1,12 @@
 /*
  * Copyright (C) 2011 OpenStack LLC.
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -15,96 +15,92 @@
  */
 package org.openstack.burrow.backend.http;
 
-import org.apache.http.HttpResponse;
-import org.openstack.burrow.client.Message;
+import static org.openstack.burrow.backend.http.BaseHttp.handleSingleMessageHttpResponse;
 
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
-import static org.openstack.burrow.backend.http.AsyncHttp.handleSingleMessageHttpResponse;
+import org.apache.http.HttpResponse;
+import org.openstack.burrow.client.Message;
 
 public class FutureMessage implements Future<Message> {
-    private HttpResponse httpResponse;
-    private Exception exception;
-    private Message message;
+  private Exception exception;
+  private HttpResponse httpResponse;
+  private Message message;
 
-    FutureMessage () {
-    }
+  FutureMessage() {
+  }
 
-    @Override
-    public boolean cancel(boolean mayInterruptIfRunning) {
-        return true;
-    }
+  @Override
+  public boolean cancel(boolean mayInterruptIfRunning) {
+    return true;
+  }
 
-    @Override
-    public boolean isCancelled() {
-        return false;
+  @Override
+  public synchronized Message get() throws InterruptedException, ExecutionException {
+    while (true) {
+      if (this.exception != null) {
+        // Something went wrong and we have an exception.
+        if (exception instanceof ExecutionException)
+          throw (ExecutionException) exception;
+        else if (exception instanceof InterruptedException)
+          throw (InterruptedException) exception;
+        else
+          throw new RuntimeException("Unexpected Exception Type: " + exception);
+      } else if (this.message != null) {
+        // There is a message already here and processed.
+        return this.message;
+      } else if (this.httpResponse != null) {
+        // We have been woken because there is a httpResponse and it has not
+        // been processed yet.
+        // try {
+        this.message = handleSingleMessageHttpResponse(this.httpResponse);
+        this.notifyAll();
+        return this.message;
+        // } catch (Exception e) {
+        // // TODO: Catch and Throw appropriate exceptions
+        // this.exception = new ExecutionException(new RuntimeException(e));
+        // this.notifyAll();
+        // throw (ExecutionException) this.exception;
+        // }
+      } else
+        this.wait();
     }
+  }
 
-    @Override
-    public synchronized boolean isDone() {
-        if (this.exception != null) {
-            return true;
-        }
-        else if (this.message != null) {
-            return true;
-        }
-        else if (this.httpResponse != null) {
-            this.message = handleSingleMessageHttpResponse(this.httpResponse);
-            this.notifyAll();
-            return true;
-        }
-        return false;
-    }
+  @Override
+  public synchronized Message get(long timeout, TimeUnit unit) throws InterruptedException,
+      ExecutionException, TimeoutException {
+    return null;
+    // TODO: Support this get() call
+  }
 
-    @Override
-    public synchronized Message get() throws InterruptedException, ExecutionException {
-        while (true) {
-            if (this.exception != null) {
-                // Something went wrong and we have an exception.
-                try {
-                    throw this.exception;
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            } else if (this.message != null) {
-                // There is a message already here and processed.
-                return this.message;
-            } else if (this.httpResponse != null) {
-                // We have been woken because there is a httpResponse and it has not been processed yet.
-                try {
-                    this.message = handleSingleMessageHttpResponse(this.httpResponse);
-                    this.notifyAll();
-                    return this.message;
-                    //TODO: Catch and Throw appropriate exceptions
-                } catch (Exception e) {
-                    this.exception = new ExecutionException(new RuntimeException("An error arose:" + e));                    this.notifyAll();
-                    try {
-                        throw this.exception;
-                    } catch (Exception ex) {
-                        e.printStackTrace();
-                    }
-                }
-            } else
-                this.wait();
-        }
-    }
+  @Override
+  public boolean isCancelled() {
+    return false;
+  }
 
-    @Override
-    public synchronized Message get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
-        return null;
-        //TODO: Support this get() call
+  @Override
+  public synchronized boolean isDone() {
+    if (this.exception != null) {
+      return true;
+    } else if (this.message != null) {
+      return true;
+    } else if (this.httpResponse != null) {
+      return true;
     }
+    return false;
+  }
 
-    public synchronized void setHttpResponse(HttpResponse response){
-        httpResponse = response;
-        this.notify();
-    }
+  public synchronized void setException(Exception e) {
+    exception = new ExecutionException(e);
+    this.notifyAll();
+  }
 
-    public synchronized void setException(Exception e) {
-        exception = e;
-        this.notify();
-    }
+  public synchronized void setHttpResponse(HttpResponse response) {
+    httpResponse = response;
+    this.notify();
+  }
 }
