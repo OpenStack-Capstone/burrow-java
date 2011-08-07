fdc84f74fdbc847940a7db3a68db8dfa2f3a4a10
Cory Kolbeck <ckolbeck@cs.pdx.edu>
2011-08-06 19:09:54 -0700

    When you make a thread, it's best to actually start it.

--- src/main/java/org/openstack/burrow/example/syslog/LogEntry.java
+++ src/main/java/org/openstack/burrow/example/syslog/LogEntry.java
@@ -1,33 +1,41 @@
 package org.openstack.burrow.example.syslog;
 
 public class LogEntry {
+    public static final int DATE_WIDTH = 15;
     private String time;
-    private String level;
+    private String host;
     private String tag;
     private String body;
 
     //TODO: Allow configuration of entry parsing via syslog format string
     //For now, assume RSYSLOG_TraditionalFileFormat
     public static LogEntry fromRawEntry(String raw) throws MalformedEntryException {
-        String[] split = raw.split(" ", 4);
-        if (split.length < 4) throw new MalformedEntryException();
+        String date = raw.substring(0, DATE_WIDTH);
+        raw = raw.substring(DATE_WIDTH);
 
-        return new LogEntry(split[0], split[1], split[2], split[3]);
+        String[] split = raw.trim().split(" *", 3);
+        if (split.length < 3) throw new MalformedEntryException();
+
+        return new LogEntry(date, split[0], split[1], split[2]);
     }
 
-    public LogEntry(String time, String level, String tag, String body) {
+    public LogEntry(String time, String host, String tag, String body) {
         this.time = time;
-        this.level = level;
+        this.host = host;
         this.tag = tag;
         this.body = body;
+    }
+
+    public String toString() {
+        return time + "//" + host + "//" + tag + "//" + body;
     }
 
     public String getTimeStamp() {
         return time;
     }
 
-    public String getLevel() {
-        return level;
+    public String getHost() {
+        return host;
     }
 
     public String getTag() {
