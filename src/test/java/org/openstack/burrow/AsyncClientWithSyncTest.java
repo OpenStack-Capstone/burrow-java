89c4ffa5e75183ba056f219b67212856c40a0bd0
Justin Wagner <jwagne@pdx.edu>
2011-08-08 15:07:14 -0700

    Removed BackendInternalException.java

---
+++ src/test/java/org/openstack/burrow/AsyncClientWithSyncTest.java
@@ -0,0 +1,11 @@
+package org.openstack.burrow;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: netbook
+ * Date: 8/8/11
+ * Time: 3:01 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AsyncClientWithSyncTest {
+}
